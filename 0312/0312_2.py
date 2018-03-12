9d364744a8589185f6155d55079636e05ef8c100
markhuang927 <markhuang294@gmail.com>
2018-03-12 21:33:28 +0800

    hw1

---
+++ 0312/0312_2.py
@@ -0,0 +1,50 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+a = 'abc'
+
+
+# In[2]:
+
+
+print(a)
+
+
+# In[5]:
+
+
+b = 3
+
+
+# In[7]:
+
+
+print(b)
+
+
+# In[6]:
+
+
+a*3
+
+
+# In[8]:
+
+
+print(a*b)
+
+
+# In[9]:
+
+
+a *= b
+
+
+# In[10]:
+
+
+print(a*b)
+
---
+++ 0312/0312_3.py
@@ -0,0 +1,56 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+num = 'b10509003'
+
+
+# In[3]:
+
+
+print(num[:])
+
+
+# In[4]:
+
+
+print(num[3:])
+
+
+# In[5]:
+
+
+print(num[:8])
+
+
+# In[6]:
+
+
+print(num[1:9])
+
+
+# In[8]:
+
+
+print(num[1:7:2])
+
+
+# In[9]:
+
+
+print(num[:7:2])
+
+
+# In[10]:
+
+
+print(num[3::3])
+
+
+# In[11]:
+
+
+print(num[::4])
+
